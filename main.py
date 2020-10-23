f6fcac35c761fca1a1102b942a3fb833a918599a
Csar Gutirrez Carrero <cesarguti47@gmail.com>
2020-10-23 13:29:09 +0200

    (P1) Added data cleaning & data analysis

--- main.py
+++ main.py
@@ -2,19 +2,42 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import nltk
 
+from nltk.tokenize import word_tokenize
+from collections import Counter
 from utils import *
 
-
+#################### LOAD DATA
 ratings_ds = pd.read_csv(DATASET_PATH)
-
-# print(ratings_ds.head())
+print(ratings_ds.head())
 
 # ratings = ratings_ds['RATING'].unique()
 
-ratings_counts = ratings_ds['RATING'].value_counts(ascending=True)
-print(ratings_counts)
+#################### DATA ANALYSIS & DATA CLEANING
+# ratings_counts = ratings_ds['RATING'].value_counts(ascending=True)
+# plot_class_balance(ratings_counts, title="Balance of ratings", filename="Balance_of_classes")
 
-fig, ax = plt.subplots()
-ratings_counts.plot(ax=ax, kind='bar', title="Balance of ratings", xlabel="Ratings", ylabel="Frecuency", rot=0)
-plt.savefig(f"{OUTPUT_PATH}/Balance_of_classes.png")
+ratings_ds_preprocess = ratings_ds.drop_duplicates(ignore_index=True)  # Drop duplicate rows keeping the first appearance and reindexing
+print(ratings_ds_preprocess.head())
+
+# ratings_counts = ratings_ds_preprocess['RATING'].value_counts()
+# plot_class_balance(ratings_counts, title="Balance of ratings (without duplicates)", filename="Balance_of_classes_no_dupl")
+
+# print(ratings_ds_preprocess.isnull().sum())  # Look for null values
+ratings_ds_preprocess = ratings_ds_preprocess.dropna()  # Drop all the rows in which any null value is present
+
+ratings_ds_preprocess['TEXT'] = ratings_ds_preprocess['TEXT'].apply(text_preprocessing)  # Apply preprocessing to every review
+print(ratings_ds_preprocess.head())
+
+# print(ratings_ds_preprocess[ratings_ds_preprocess['TEXT'] == ""])  # Look for empty strings
+ratings_ds_preprocess['TEXT'].replace("", np.nan, inplace=True)  # Replace empty str by NaN so we can drop them
+# print(ratings_ds_preprocess.isnull().sum())
+ratings_ds_preprocess = ratings_ds_preprocess.dropna()  # Drop all the rows in which any null value is present
+
+# ratings_counts = ratings_ds_preprocess['RATING'].value_counts()
+# plot_class_balance(ratings_counts, title="Balance of ratings (after cleaning)", filename="Balance_of_classes_aft_clean")
+
+most_common_words_per_rating = ratings_ds_preprocess.groupby('RATING')['TEXT'].apply(lambda review: Counter(" ".join(review).split()).most_common(5))
+most_common_words_per_rating.to_csv(f"./{OUTPUT_PATH}/most_common_words_per_rating.txt")
+
--- utils.py
+++ utils.py
@@ -1,4 +1,49 @@
+import matplotlib.pyplot as plt
+import string
+import re
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+
+
 #################### PARAMETERS
 
 DATASET_PATH = "./Resources/ratings.csv"
 OUTPUT_PATH = "./Output"
+
+
+def plot_class_balance(data, title, filename):
+    fig, ax = plt.subplots()
+    data.plot(ax=ax, kind='bar', title=title, xlabel="Ratings", ylabel="Frecuency", rot=0)
+    plt.savefig(f"{OUTPUT_PATH}/{filename}.png")
+
+def text_preprocessing(text):
+    # Lowercase the text to reduce the vocabulary of our data
+    text = text.lower()
+
+    # Remove numbers. Not necessary to convert them into text
+    text = re.sub(r'\d+', '', text)
+
+    # Remove punctuations so we don’t have different forms of the same word
+    text = text.translate( str.maketrans('', '', string.punctuation) )
+
+    # Remove words that do not contribute to the meaning of a sentence (stop-words)
+    stop_words = set(stopwords.words("english"))
+    word_tokens = word_tokenize(text)
+    text_tokens = [word for word in word_tokens if word not in stop_words]
+
+    # Apply stemming to reduce words to their root form
+    # stemmer = PorterStemmer()
+    # text_tokens = [stemmer.stem(word) for word in text_tokens]
+
+    # Apply lemmatization to reduce words to their root form ensuring that the root word belongs to the language
+    lemmatizer = WordNetLemmatizer()
+    text_tokens = [lemmatizer.lemmatize(word, pos ='v') for word in text_tokens]
+
+    # Remove all the white spaces in a string
+    text = " ".join(text_tokens)
+
+    return text
